759bb3be7c07c01cf4aa434fffdd0ea89448fbb1
risingstar2018 <33366277+risingstar2018@users.noreply.github.com>
2021-07-19 23:07:31 +0800

    fork from artdefi-contracts

---
+++ truffle-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  // Uncommenting the defaults below
+  // provides for an easier quick-start with Ganache.
+  // You can also follow this format for other networks;
+  // see <http://truffleframework.com/docs/advanced/configuration>
+  // for more details on how to specify configuration options!
+  //
+  contracts_build_directory: "./build",
+  contracts_directory: "./Vault2",
+  networks: {
+   development: {
+     host: "127.0.0.1",
+     port: 7545,
+     network_id: "*"
+   },
+  },  
+  compilers: {
+    solc: {
+      version: "0.6.12",
+      settings: {
+        optimizer: {
+          enabled: true,
+        },
+      },
+    }
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
+  }
+};
